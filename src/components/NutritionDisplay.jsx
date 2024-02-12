73bb9f94df57a8a2e53991346d28363531bfa956
tarikmetin <tarik.metin1@gmail.com>
2024-02-12 18:00:20 -0300

    pre-doughnut

--- src/components/NutritionDisplay.jsx
+++ src/components/NutritionDisplay.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import PieChart from "./charts/PieChart";
+import BarChart from "./charts/BarChart";
 
 export default function NutritionDisplay({ nutritionValues }) {
   const [totalVal, setTotalVal] = useState({
@@ -7,11 +8,6 @@ export default function NutritionDisplay({ nutritionValues }) {
     totalCarbohydrate: 0,
     totalFat: 0,
     totalCal: 0,
-  });
-
-  const [displayData, setDisplayData] = useState({
-    labels: ["Proteins", "Carbs", "Fats"],
-    datasets: [],
   });
 
   useEffect(() => {
@@ -31,33 +27,19 @@ export default function NutritionDisplay({ nutritionValues }) {
     });
   }, [nutritionValues]);
 
-  useEffect(() => {
-    setDisplayData({
-      ...displayData,
-      datasets: [
-        {
-          label: "Weight in grams",
-          data: [
-            totalVal.totalProtein,
-            totalVal.totalCarbohydrate,
-            totalVal.totalFat,
-          ],
-          backgroundColor: ["#F29544", "#2A2640", "#A64E46"],
-        },
-      ],
-    });
-  }, [totalVal]);
-
   return (
     <div className="display-area">
-      <div className="text-info">
+      {/* <div className="text-info">
         <h1>Total Protein: {totalVal.totalProtein} gr</h1>
         <h1>Total Carbohydrates: {totalVal.totalCarbohydrate} gr</h1>
         <h1>Total Fat: {totalVal.totalFat} gr</h1>
         <h1>Total Calories: {totalVal.totalCal} cal</h1>
+      </div> */}
+      <div className="chart-info bar">
+        <BarChart totalVal={totalVal} />
       </div>
-      <div className="chart-info">
-        <PieChart chartData={displayData} />
+      <div className="chart-info pie">
+        <PieChart totalVal={totalVal} />
       </div>
     </div>
   );
--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -1,8 +1,66 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { useState, useEffect } from "react";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-export default function BarChart({ chartData }) {
-  return <Bar data={chartData} />;
+const options = {
+  plugins: {
+    legend: false,
+  },
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+    },
+  },
+};
+
+export default function BarChart({ totalVal }) {
+  const [displayData, setDisplayData] = useState({
+    labels: ["Proteins", "Carbs", "Fats", "Calories"],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    setDisplayData({
+      ...displayData,
+      datasets: [
+        {
+          label: "Nutrition Values",
+          data: [
+            totalVal.totalProtein,
+            totalVal.totalCarbohydrate,
+            totalVal.totalFat,
+            totalVal.totalCal,
+          ],
+          backgroundColor: ["#F29544", "#2A2640", "#A64E46", "#F7DC6F"],
+        },
+      ],
+    });
+  }, [totalVal]);
+
+  return <Bar data={displayData} options={options} />;
 }
